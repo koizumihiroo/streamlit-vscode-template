e4ccc4ee74f4cf25e8364117f9865746d805a7b6
hkoizumi <koizumi.hiroo@justincase.jp>
2021-02-16 16:40:32 +0900

    Add first files

---
+++ README.md
@@ -0,0 +1,81 @@
+## Prerequisite
+
+<details markdown="1">
+<summary>set <code>pyenv</code></summary>
+
+### On Mac OS
+
+installation
+
+`~/.bashrc` match your situation like `~/.bash_profile`.
+
+```sh
+PY_VERSION=3.8.7
+brew install pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+exec $SHELL -l # reload
+pyenv install $PY_VERSION
+pyenv local $PY_VERSION
+```
+
+### On Ubuntu or other OSs
+
+No documents. You can google it.
+
+</details>
+
+<details markdown="1">
+<summary>set <code>poetry</code></summary>
+
+### On Mac OS
+
+installation
+
+```sh
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+```
+
+```sh
+poetry config virtualenvs.in-project true
+poetry init
+# Would you like to define your main dependencies interactively? (yes/no) no
+# Would you like to define your development dependencies interactively? (yes/no) [yes] no
+# Do you confirm generation? (yes/no) [yes] 
+```
+
+```sh
+# add package by poetry
+poetry add streamlit watchdog
+```
+
+</details>
+
+<details markdown="1">
+<summary>other setting</summary>
+
+```sh
+echo -e ".venv\n__pycache__\n.mypy_cache/\n.git" > .gitignore
+echo -e ".venv\n__pycache__\n.mypy_cache/" > .dockerignore
+```
+
+</details>
+
+## Run streamlit demo
+
+```sh
+poetry install # only when package is added or updated
+poetry shell
+streamlit hello
+# See left side bar, select DataFrame demo, then code will be shown in main panel.
+```
+
+## Run own code in local
+
+```sh
+poetry shell
+streamlit run src/main.py 
+```
+
+## Run in docker
---
+++ src/main.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+
+# This code is mainly copied from `streamlit hello` DataFrame demo.
+
+@st.cache
+def get_UN_data():
+    AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
+    df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
+    return df.set_index("Region")
+
+
+try:
+    st.set_page_config(
+        layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+        initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
+        page_title=None,  # String or None. Strings get appended with "• Streamlit".
+        page_icon=None,  # String, anything supported by st.image, or None.
+    )
+    df = get_UN_data()
+    countries = st.multiselect(
+        "Choose countries", list(df.index), ["China", "United States of America"]
+    )
+    if not countries:
+        st.error("Please select at least one country.")
+    else:
+        data = df.loc[countries]
+        data /= 1000000.0
+        st.write("### Gross Agricultural Production ($B)", data.sort_index())
+
+        data = data.T.reset_index()
+        data = pd.melt(data, id_vars=["index"]).rename(
+            columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
+        )
+        chart = (
+            alt.Chart(data)
+            .mark_area(opacity=0.3)
+            .encode(
+                x="year:T",
+                y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
+                color="Region:N",
+            )
+        )
+        st.altair_chart(chart, use_container_width=True)
+except urllib.error.URLError as e:
+    st.error(
+        """
+        **This demo requires internet access.**
+
+        Connection error: %s
+    """
+        % e.reason
+    )
